6cab82b99fad2a9c681006cc4c18f85570c94958
Marian Vittek <vittek@xrefactory.com>
2024-11-09 17:53:03 +0100

    readme

--- hello-world/helloWorld.py
+++ hello-world/helloWorld.py
@@ -2,13 +2,16 @@ import torch # torch provides basic functions, from setting a random seed (for r
 import torch.nn as nn # torch.nn allows us to create a neural network.
 import torch.nn.functional as F # nn.functional give us access to the activation and loss functions.
 
-## create a neural network class by creating a class that inherits from nn.Module.
+## create a "neural network" class by creating a class that inherits from nn.Module.
+## In fact, in this case the class is not a neural network at all. We simply construct 'hello world'
+## message in it.
+
 class NeuralNetwork(nn.Module):
     
     def __init__(self): 
         super().__init__() # initialize an instance of the parent class, nn.Model.
 
-    ## forward() is the main function executed inside IMX500. It takes an input image frame (tensor(3,224,224))
+    ## forward() is the main function executed inside IMX500. It takes an input image frame (tensor(224,224,3))
     ## and runs it through the "neural network". It returns a tensor which is returned to RPi in metadata.
     def forward(self, input): 
         # It seems that I can not simply return constant "hello world" tensor, I have to 'compute' it from the input
@@ -24,6 +27,6 @@ class NeuralNetwork(nn.Module):
         zz[0:len(text)] = torch.tensor(text)
         # add the tensor containing "Hello world" message to zeroed input
         oo = torch.add(oo, zz);
-        # return the 1D tensor. In general I can return 2D or 3D tensor as well.
+        # return the 1D tensor with the message. In general I can return 2D or 3D tensor as well.
         return oo
 
